b35a334054cb4fa200a5160a0826471e024fb743
Selinna <selinnalin0705@gmail.com>
2025-04-28 16:31:22 -0400

    yolo added

--- final_challenge2025/final_challenge2025/shrinkray_heist/model/detection_node.py
+++ final_challenge2025/final_challenge2025/shrinkray_heist/model/detection_node.py
@@ -5,12 +5,15 @@ from cv_bridge import CvBridge
 
 from sensor_msgs.msg import Image
 from .detector import Detector
+import os 
+import numpy as np
+import cv2
 
 class DetectorNode(Node):
     def __init__(self):
         super().__init__("detector")
         self.detector = Detector()
-        self.publisher = None #TODO
+        self.publisher = self.create_publisher(Image, "/detector/annotated_img", 1)
         self.subscriber = self.create_subscription(Image, "/zed/zed_node/rgb/image_rect_color", self.callback, 1)
         self.bridge = CvBridge()
 
@@ -20,7 +23,20 @@ class DetectorNode(Node):
         # Process image with CV Bridge
         image = self.bridge.imgmsg_to_cv2(img_msg, "bgr8")
 
-        #TODO: 
+        # Run yolo on this image 
+        results = self.detector.predict(image)
+        predictions = results["predictions"]
+        original_image = results["original_image"]
+
+        out = self.detector.draw_box(original_image, predictions, draw_all=True)
+
+        rgb_np = np.array(out)
+        bgr_np = cv2.cvtColor(rgb_np, cv2.COLOR_RGB2BGR)
+        ros_img = self.bridge.cv2_to_imgmsg(bgr_np, "bgr8")
+        ros_img.header = img_msg.header
+
+        # Publish the image
+        self.publisher.publish(ros_img)
 
 def main(args=None):
     rclpy.init(args=args)
--- final_challenge2025/setup.py
+++ final_challenge2025/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'detector = final_challenge2025.shrinkray_heist.model.detection_node:main',
         ],
     },
 )
