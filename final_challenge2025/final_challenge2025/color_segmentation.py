6a52af2b025ade5ca6aa0fe9717c0263ab3be091
rengzheng <rzhengcontacts@gmail.com>
2025-05-02 19:55:09 +0000

    some cleanup

--- final_challenge2025/final_challenge2025/color_segmentation.py
+++ final_challenge2025/final_challenge2025/color_segmentation.py
@@ -30,8 +30,7 @@ def cd_color_segmentation(img, template):
 		img: np.3darray; the input image with an object to be detected. BGR.
 		template_file_path; Not required, but can optionally be used to automate setting hue filter values.
 	Return:
-		bbox: ((x1, y1), (x2, y2)); the bounding box of the cone, unit in px
-				(x1, y1) is the top left of the bbox and (x2, y2) is the bottom right of the bbox
+		Return lines and y-coordinate of the cropped image
 	"""
 
 	# crop the image to focus on the lower half
@@ -65,7 +64,9 @@ def cd_color_segmentation(img, template):
 	
     # Line detection
 	# rho is in pixels, theta is in radians
-	lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi/180, threshold=50, minLineLength=15, maxLineGap=10)
+	lines = cv2.HoughLinesP(
+		edges, rho=1, theta=np.pi/180, threshold=50, minLineLength=15, maxLineGap=10
+	)
 	
 	filtered_lines = []
 	threshold_angle = 23	# in degrees (tune this value if needed)
@@ -80,19 +81,6 @@ def cd_color_segmentation(img, template):
 		
 		if angle > threshold_angle:
 			filtered_lines.append([[x1, y1, x2, y2]])
-	
-    # draw the line
-	# if lines is not None: 
-	# 	for line in lines:
-	# 		x1, y1, x2, y2 = line[0]
-	# 		y1 += crop_y_start
-	# 		y2 += crop_y_start
-	# 		cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
-
-	# cv2.imshow("Segmented Output", result)
-	# cv2.imshow("image", img)
-	# cv2.waitKey(0)
-	# cv2.destroyAllWindows()
 
 	# Return lines and y-coordinate of the cropped image
 	return filtered_lines, crop_y_start, result
