c209320b160ef9b62c37de2e5cc26ecc74e5e6d3
rengzheng <rzhengcontacts@gmail.com>
2025-05-08 01:15:09 +0000

    yaw fix

--- final_challenge2025/final_challenge2025/moon_race/race_control.py
+++ final_challenge2025/final_challenge2025/moon_race/race_control.py
@@ -4,6 +4,7 @@ import rclpy
 from rclpy.node import Node
 # Driving.
 from ackermann_msgs.msg import AckermannDriveStamped
+from nav_msgs.msg import Odometry
 
 # Numpy
 import numpy as np
@@ -23,6 +24,9 @@ class PurePursuit(Node):
         # Topics to be used.
         self.declare_parameter('drive_topic', "default")
         self.drive_topic: str = self.get_parameter('drive_topic').get_parameter_value().string_value
+        self.declare_parameter('odom_topic', "default")
+        self.odom_topic: str = self.get_parameter('odom_topic').get_parameter_value().string_value
+        self.last_odom_timestamp: float = None
 
         # Pure Pursuit parameters.
         self.declare_parameter('lookahead', 1.2)
@@ -68,6 +72,10 @@ class PurePursuit(Node):
         self.midpoint_sub = self.create_subscription(
             WorldTrajInfo, "/trajectory/midpoint",
             self.trajectory_callback, 1
+        )
+        self.odom_sub = self.create_subscription(
+            Odometry, self.odom_topic,
+            self.real_time_kinematics, 1
         )
         self.drive_pub = self.create_publisher(
             AckermannDriveStamped, self.drive_topic, 1
@@ -115,23 +123,38 @@ class PurePursuit(Node):
             goal_point = relative_positions[-1]
         return goal_point
 
-    def real_time_kinematics(self, steering_angle: float, speed: float):
+    def real_time_kinematics(self, odometry: Odometry):
         """
         Evolve the stored trajectory by moving the vehicle forward by
         drive_distance along its x‑axis and rotating the frame by the
         small yaw change from the bicycle model.
         """
+         # calculate the change in time (dt)
+        curr_time: float = odometry.header.stamp.sec + odometry.header.stamp.nanosec * 1e-9
+
+        if self.last_odom_timestamp is None:
+            self.last_odom_timestamp = curr_time
+            return
+        
+        dt: float = curr_time - self.last_odom_timestamp
+        self.last_odom_timestamp = curr_time
+
+        # get odometry data 
+        odom: npt.NDArray[np.float] = np.array(
+            [odometry.twist.twist.linear.x, # vx
+             odometry.twist.twist.linear.y, # vy
+             odometry.twist.twist.angular.z]# yaw
+        )
+
         # how far we move in this timestep
-        drive_distance = speed * self.refresh_rate
-        # yaw change = v/L * tan(delta) * dt
-        delta_yaw = drive_distance * np.tan(steering_angle) / self.wheelbase_length
+        yaw = odom[2] * dt
 
         with self.trajectory_lock:
             # translate every point backward by drive_distance along the x‑axis
             # since points are in the vehicle frame
-            self.traj_pts[:, 0] -= drive_distance
+            self.traj_pts -= odom[:2] * dt
             # rotation matrix for a frame rotation of -delta_yaw:
-            c, s = np.cos(delta_yaw), np.sin(delta_yaw)
+            c, s = np.cos(yaw), np.sin(yaw)
             R = np.array([
                 [c, -s],
                 [s, c]
@@ -233,9 +256,6 @@ class PurePursuit(Node):
         #     f"deviation: {self.deviation:.2f}"
         # )
         self.publish_drive_cmd(speed, steering_angle)
-
-        # Update the trajectory given the commanded motion.
-        self.real_time_kinematics(steering_angle, speed) 
 
     def trajectory_callback(self, msg: WorldTrajInfo):
         """
